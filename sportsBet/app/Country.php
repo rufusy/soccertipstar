4f566a0d54c6a717c2e29e23f7d23926f2720dcc
rufusy <ruffiam@yahoo.com>
2019-11-04 17:34:01 +0300

    New models

---
+++ sportsBet/app/Country.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    public function leagues()
+    {
+        return $this->hasMany(League::class);
+    }
+}
