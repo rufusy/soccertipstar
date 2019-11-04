4dad60d6dc80f6dc053a46ac94e762082e701c55
rufusy <ruffiam@yahoo.com>
2019-11-04 17:35:40 +0300

    Migrations for countries, teams and leagues tables

---
+++ sportsBet/database/migrations/2019_11_01_174437_create_leagues_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeaguesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leagues', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('country_id');
+
+            $table->string('name')->unique();
+            $table->timestamps();
+
+            $table->foreign('country_id')
+                ->references('id')
+                ->on('countries')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leagues');
+    }
+}
---
+++ sportsBet/database/migrations/2019_11_01_174450_create_teams_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('league_id');
+            $table->string('name')->unique();
+            $table->timestamps();
+
+            $table->foreign('league_id')
+                ->references('id')
+                ->on('leagues')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
